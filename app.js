8d5d6046c7501b064a3e15036afa4405ad4e904c
unknown <wyes89122@gmail.com>
2020-04-29 22:39:29 +0800

    Prepared app for deployment

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 
+const compression = require('compression');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -65,6 +67,8 @@ app.use(
     ]
   })
 );
+
+app.use(compression());
 
 // Test middleware
 app.use((req, res, next) => {
